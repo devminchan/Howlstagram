dfddd4a00319b5ef74216cd2ce8e89af18c2db41
minchanKim <syubsyubboy@gmail.com>
2019-11-12 11:36:33 +0900

    Complete email login feature

--- app/src/main/java/com/syubsyubboy/howlstagram/LoginActivity.kt
+++ app/src/main/java/com/syubsyubboy/howlstagram/LoginActivity.kt
@@ -1,12 +1,60 @@
 package com.syubsyubboy.howlstagram
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        email_login_button.setOnClickListener {
+            signinAndSignup()
+        }
+    }
+
+    fun signinAndSignup() {
+        val email = email_edittext.text.toString()
+        val password = password_edittext.text.toString()
+
+        auth?.createUserWithEmailAndPassword(email, password)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    moveMainPage(task.result?.user)
+                } else if (task.exception?.message.isNullOrEmpty()) {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                } else {
+                    signinEmail()
+                }
+            }
+    }
+
+    fun signinEmail() {
+        val email = email_edittext.text.toString()
+        val password = password_edittext.text.toString()
+
+        auth?.signInWithEmailAndPassword(email, password)?.addOnCompleteListener {
+            if (it.isSuccessful) {
+                moveMainPage(it.result?.user)
+            } else {
+                Toast.makeText(this, it.exception?.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
